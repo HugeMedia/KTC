3aa39262365d6a9158707c0ee878dcd1c4d02bca
William Wolfrath <william.wolfrath@gmail.com>
2011-07-22 10:00:35 -0400

    slogan, javascript files, and cufon replacement

--- sites/all/themes/ktc/templates/page/page.tpl.php
+++ sites/all/themes/ktc/templates/page/page.tpl.php
@@ -76,14 +76,9 @@
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
         </a>
       <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
           <?php if ($site_slogan): ?>
             <div id="site-slogan"><?php print $site_slogan; ?></div>
           <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
-      <?php endif; ?>
 
       <?php print render($page['header']); ?>
 
@@ -91,6 +86,8 @@
 
     <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section">
+        <?php //dpm($page); ?>
+        <?php print render($page['navigation']); ?>
         <?php //print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
         <?php //print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
       </div></div> <!-- /.section, /#navigation -->
@@ -130,5 +127,11 @@
     <div id="footer"><div class="section">
       <?php print render($page['footer']); ?>
     </div></div> <!-- /.section, /#footer -->
-
+<script type="text/javascript">
+    Cufon.replace('#site-slogan');
+    Cufon.replace('p');
+    Cufon.replace('h2');
+    Cufon.replace('#superfish-1 a');
+    Cufon.now();
+</script> 
   </div></div> <!-- /#page, /#page-wrapper -->
