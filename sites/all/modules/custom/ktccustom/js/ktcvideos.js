dcc167cf8230205ec39850a70e767a4534dfc440
William Wolfrath <william.wolfrath@gmail.com>
2011-08-26 05:47:47 -0400

    changed tax name

--- sites/all/modules/custom/ktccustom/js/ktcvideos.js
+++ sites/all/modules/custom/ktccustom/js/ktcvideos.js
@@ -4,7 +4,7 @@
         
         Cufon.replace('h4');
         Cufon.replace('#videos-top p');
-        Cufon.replace('#views_slideshow_cycle_main_video_categories-block div.views-field-name a');
+        Cufon.replace('#views_slideshow_cycle_main_video_categories-block div.views-field-name span');
     
     });
 	
