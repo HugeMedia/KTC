3f8564a2491d6019392fec00a83f18ebd1e5d9ca
William Wolfrath <william.wolfrath@gmail.com>
2011-08-26 02:07:48 -0400

    removed debug

--- sites/all/modules/views_slideshow/theme/views-slideshow-controls-text-pause.tpl.php
+++ sites/all/modules/views_slideshow/theme/views-slideshow-controls-text-pause.tpl.php
@@ -1,2 +1,2 @@
-<?php dpm($variables); ?>
+<?php //dpm($variables); ?>
 <span id="views_slideshow_controls_text_pause_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><?php print $start_text; ?></a></span>
