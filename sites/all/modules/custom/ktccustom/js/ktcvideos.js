06e8c5c55015dd87c499ffb7bedd4be414ba24ea
William Wolfrath <william.wolfrath@gmail.com>
2011-08-25 22:21:40 -0400

    more for soda stories page

--- sites/all/modules/custom/ktccustom/js/ktcvideos.js
+++ sites/all/modules/custom/ktccustom/js/ktcvideos.js
@@ -4,6 +4,7 @@
         
         Cufon.replace('h4');
         Cufon.replace('#videos-top p');
+        Cufon.replace('#views_slideshow_cycle_main_video_categories-block div.views-field-name a');
     
     });
 	
