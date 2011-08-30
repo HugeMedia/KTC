247a0fbd9535da0e43c9ecdf31d66af013662833
William Wolfrath <william.wolfrath@gmail.com>
2011-08-30 03:41:57 -0400

    syntax

--- sites/all/modules/custom/ktccustom/tmpl/ktc-facts.tpl.php
+++ sites/all/modules/custom/ktccustom/tmpl/ktc-facts.tpl.php
@@ -5,7 +5,7 @@
             <?php //dd($fact); ?>
             <?php if (isset($fact->text)): ?>
                 <div class="ktc-fact fact-<?php print $fact->index; ?>" type="<?php print $fact->type; ?>"><div class="fact-text"><?php print $fact->text;  ?></div>
-                <div class="ktc-learn-more"><?php print l('Learn More', $fact->url); ?></div><div class="ktc-fact-tweet"> <a href="http://twitter.com/share?url=<?php print $fact->url; ?>" target="_blank">Tweet this.</a></div></div>
+                <div class="ktc-learn-more"><?php print l('Learn More.', $fact->url); ?></div><div class="ktc-fact-tweet"> <a href="http://twitter.com/share?url=<?php print $fact->url; ?>" target="_blank">Tweet this.</a></div></div>
             <?php endif; ?>
         <?php endforeach; ?>
     </div>
