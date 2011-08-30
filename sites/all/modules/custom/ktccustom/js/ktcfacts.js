9404adaff7c695a1cf8e46aa236b185a60bef1f1
William Wolfrath <william.wolfrath@gmail.com>
2011-08-30 02:54:00 -0400

    learn more and tweet links for fast facts

--- sites/all/modules/custom/ktccustom/js/ktcfacts.js
+++ sites/all/modules/custom/ktccustom/js/ktcfacts.js
@@ -12,8 +12,19 @@
 	    $('#label-news').hide();
 	    $('#label-fact').show();
 	}
+	
+	console.log($(this));
+	var fact_text = $(this).find('.fact-text');
+	var learn_more = $(this).find('.ktc-learn-more a');
+	var fact_tweet = $(this).find('.ktc-fact-tweet');
 	//console.log($(this).attr('style'));
-	Cufon.replace(this);
+	
+	Cufon.replace(fact_text);
+	Cufon.replace(learn_more);
+	Cufon.replace(fact_tweet);
+	//Cufon.replace(this + ' .fact-text');
+	//Cufon.replace(this + ' .ktc-learn-more a');
+	//Cufon.replace(this + ' .ktc-fact-tweet');
     }
     
     $(document).ready(function() {
@@ -22,13 +33,17 @@
 	
         $('#facts-listing').cycle({ 
 	    fx:    'fade',
-	    width: 700,
+	    width: 854,
 	    speed: 'slow',
 	    timeout: 5000,
 	    fit: 1,
 	    before: preFact,
 	    pause:  1 
 	});    
+    
+    Cufon.replace('.fact-text:first');
+    Cufon.replace('.ktc-learn-more:first a');
+    Cufon.replace('.ktc-fact-tweet');
     
     });
 	
--- sites/all/modules/custom/ktccustom/tmpl/ktc-facts.tpl.php
+++ sites/all/modules/custom/ktccustom/tmpl/ktc-facts.tpl.php
@@ -2,8 +2,10 @@
     <div id="facts-label"><div id="label-fact">Fast Fact:</div><div id="label-news">News Flash:</div></div>
     <div id="facts-listing">
         <?php foreach ($element['facts'] as $fact): ?>
+            <?php //dd($fact); ?>
             <?php if (isset($fact->text)): ?>
-                <div class="ktc-fact fact-<?php print $fact->index; ?>" type="<?php print $fact->type; ?>"><div class="fact-text"><?php print $fact->text;  ?></div></div>
+                <div class="ktc-fact fact-<?php print $fact->index; ?>" type="<?php print $fact->type; ?>"><div class="fact-text"><?php print $fact->text;  ?></div>
+                <div class="ktc-learn-more"><?php print l('Learn More', $fact->url); ?></div><div class="ktc-fact-tweet"> <a href="http://twitter.com/share?url=<?php print $fact->url; ?>" target="_blank">Tweet this.</a></div></div>
             <?php endif; ?>
         <?php endforeach; ?>
     </div>
